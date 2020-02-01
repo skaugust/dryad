8fe22c6590a9e68e0b9c9a40007aa71e17a31ad4
skaugust <skyleram@gmail.com>
2020-02-01 08:05:19 -0800

    WSAD to move dryad, simple masks to show nature/pollution.

---
+++ Assets/Scripts/ScalingSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalingSize : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x + Time.deltaTime, transform.localScale.y + Time.deltaTime, transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/SimpleDryadMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDryadMovement : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector2.right;
+        }
+        Vector2 movementDelta = movement.normalized * Time.deltaTime * speed;
+        this.transform.position += new Vector3(movementDelta.x, movementDelta.y, 0);
+    }
+}
