1402fad940a04b433ca5e3618c8bc61c1fb297ac
skaugust <skyleram@gmail.com>
2020-02-01 09:24:52 -0800

    Added factories that can be destroyed by clicking on them.

---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
