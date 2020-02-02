532131d98d07d8ff94079cd1abd91d87aec9d75e
skaugust <skyleram@gmail.com>
2020-02-02 15:52:02 -0800

    Add manabar.

---
+++ Assets/Scripts/Manabar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manabar : MonoBehaviour
+{
+    private const int MAX_MANA_SCALE = 400;
+    private const int MAX_MANA_VALUE = 10;
+
+    private BalanceManager manaSource;
+
+    void Start()
+    {
+        manaSource = FindObjectOfType<BalanceManager>();
+    }
+
+    void Update()
+    {
+        float percent = manaSource.mana / MAX_MANA_VALUE;
+        float manaScale = percent * MAX_MANA_SCALE;
+        this.transform.localScale = new Vector3(manaScale, this.transform.localScale.y, 1);
+    }
+}
