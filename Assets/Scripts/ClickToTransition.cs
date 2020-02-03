d39dc2efc285b41ba3f0c042e0092da6d4aa3f33
skaugust <skyleram@gmail.com>
2020-02-02 16:25:44 -0800

    Make the title scene work.

---
+++ Assets/Scripts/ClickToTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToTransition : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("IntroScene");
+        }
+    }
+}
